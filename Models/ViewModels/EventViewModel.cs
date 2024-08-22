734a686c7a8431c80e8f60192af33e4eb4536fba
MaamounHajtaher <151000787+MaamounHajtaher@users.noreply.github.com>
2024-08-22 21:53:13 +0300

    added event viewModel

---
+++ Models/ViewModels/EventViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace event_management_asp_project.Models.ViewModels
+{
+    public class EventViewModel
+    {
+        //Search Attribute
+        [MaxLength(30)]
+        public string? Title { get; set; }
+
+        //Search Results
+        public IEnumerable<Event>? Events { get; set; }
+    }
+}
