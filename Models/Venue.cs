44e66a4352cba5681b5ca8b9ffd2480e43f0380d
Omar Ajaj <omar.aj.business@gmail.com>
2024-08-23 19:26:24 +0300

    Change Display name of Venue name from "Venues" to "Venue Name"

--- Models/Venue.cs
+++ Models/Venue.cs
@@ -11,7 +11,7 @@ namespace event_management_asp_project.Models
         [Required]
         [MaxLength(30)]
         [Column(TypeName = "nvarchar(30)")]
-        [Display(Name = "Venues")]
+        [Display(Name = "Venue Name")]
         public string Name { get; set; } = string.Empty;
 
         [Required]
