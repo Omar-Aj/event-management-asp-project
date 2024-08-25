a37a18ba0880e60d91f33c9e529f4296c85b4a67
Omar Ajaj <omar.aj.business@gmail.com>
2024-08-25 12:03:39 +0300

    Remove Reservation Pages, Clean Reservation Controller.

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -19,41 +19,6 @@ namespace event_management_asp_project.Controllers
             _context = context;
         }
 
-        // GET: Reservations
-        public async Task<IActionResult> Index()
-        {
-            var applicationDbContext = _context.tblReservations.Include(r => r.Event).Include(r => r.Venue);
-            return View(await applicationDbContext.ToListAsync());
-        }
-
-        // GET: Reservations/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reservation = await _context.tblReservations
-                .Include(r => r.Event)
-                .Include(r => r.Venue)
-                .FirstOrDefaultAsync(m => m.ReservationId == id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-
-            return View(reservation);
-        }
-
-        // GET: Reservations/Create
-        public IActionResult Create()
-        {
-            ViewData["EventId"] = new SelectList(_context.tblEvents, "EventId", "Description");
-            ViewData["VenueId"] = new SelectList(_context.tblVenues, "VenueId", "Location");
-            return View();
-        }
-
         // POST: Reservations/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -67,84 +32,7 @@ namespace event_management_asp_project.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Details", "MyEvents", new {id = reservation.EventId});
             }
-            ViewData["EventId"] = new SelectList(_context.tblEvents, "EventId", "Description", reservation.EventId);
-            ViewData["VenueId"] = new SelectList(_context.tblVenues, "VenueId", "Location", reservation.VenueId);
-            return View(reservation);
-        }
-
-        // GET: Reservations/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reservation = await _context.tblReservations.FindAsync(id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-            ViewData["EventId"] = new SelectList(_context.tblEvents, "EventId", "Description", reservation.EventId);
-            ViewData["VenueId"] = new SelectList(_context.tblVenues, "VenueId", "Location", reservation.VenueId);
-            return View(reservation);
-        }
-
-        // POST: Reservations/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ReservationId,ReservationDate,VenueId,EventId")] Reservation reservation)
-        {
-            if (id != reservation.ReservationId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(reservation);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ReservationExists(reservation.ReservationId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["EventId"] = new SelectList(_context.tblEvents, "EventId", "Description", reservation.EventId);
-            ViewData["VenueId"] = new SelectList(_context.tblVenues, "VenueId", "Location", reservation.VenueId);
-            return View(reservation);
-        }
-
-        // GET: Reservations/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reservation = await _context.tblReservations
-                .Include(r => r.Event)
-                .Include(r => r.Venue)
-                .FirstOrDefaultAsync(m => m.ReservationId == id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-
-            return View(reservation);
+            return RedirectToAction("Index", "MyEvents");
         }
 
         // POST: Reservations/Delete/5
