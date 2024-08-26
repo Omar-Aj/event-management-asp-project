35ac753716a557e8189345680aed3fb1caa520ce
MaamounHajtaher <151000787+MaamounHajtaher@users.noreply.github.com>
2024-08-26 12:42:09 +0300

    clean guestsController, deleted guests views, and deleted delete view of myEvents

--- Controllers/GuestsController.cs
+++ Controllers/GuestsController.cs
@@ -19,37 +19,6 @@ namespace event_management_asp_project.Controllers
             _context = context;
         }
 
-        // GET: Guests
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.tblGuests.ToListAsync());
-        }
-
-        // GET: Guests/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var guest = await _context.tblGuests
-                .FirstOrDefaultAsync(m => m.GuestId == id);
-            if (guest == null)
-            {
-                return NotFound();
-            }
-
-            return View(guest);
-        }
-
-        // GET: Guests/Create
-        public IActionResult Create()
-        {
-
-			return PartialView("_GuestRegisterEvent");
-		}
-
 		// POST: Guests/Create
 		// To protect from overposting attacks, enable the specific properties you want to bind to.
 		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -67,74 +36,40 @@ namespace event_management_asp_project.Controllers
 			return RedirectToAction("Details", "Events", new {id = guest.EventId});
         }
 
-        // GET: Guests/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var guest = await _context.tblGuests.FindAsync(id);
-            if (guest == null)
-            {
-                return NotFound();
-            }
-            return View(guest);
-        }
-
         // POST: Guests/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("GuestId,Name,Email,ConfirmEmail,Phone")] Guest guest)
-        {
-            if (id != guest.GuestId)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("GuestId,Name,Email,ConfirmEmail,Phone")] Guest guest)
+        //{
+        //    if (id != guest.GuestId)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(guest);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!GuestExists(guest.GuestId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(guest);
-        }
-
-        // GET: Guests/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var guest = await _context.tblGuests
-                .FirstOrDefaultAsync(m => m.GuestId == id);
-            if (guest == null)
-            {
-                return NotFound();
-            }
-
-            return View(guest);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(guest);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!GuestExists(guest.GuestId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(guest);
+        //}
 
         // POST: Guests/Delete/5
         [HttpPost, ActionName("Delete")]
